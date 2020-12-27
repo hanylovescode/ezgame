f366b7c39c87f6780726cd40106e616f7deaff3f
z3tr <m5tfi@pm.me>
2020-12-27 15:51:51 +0200

    using a dict for imported fonts

--- src/gui.py
+++ src/gui.py
@@ -2,13 +2,13 @@ import pygame
 
 
 class GUI:
-    def __init__(self, font: pygame.font.Font):
-        self.font = font
+    def __init__(self, fonts: dict):
+        self.fonts = fonts
         self.fps_pos = 5, 5
 
     def show_fps(self, game_surface: pygame.Surface, fps: float):
         fps_str = str(int(fps))
-        fps_txt = self.font.render(fps_str, True, pygame.Color("coral"))
+        fps_txt = self.fonts['fps'].render(fps_str, True, pygame.Color("coral"))
         game_surface.blit(fps_txt, self.fps_pos)
 
     def update(self, game_surface: pygame.Surface, show_fps: bool, fps: float):
