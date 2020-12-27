66787e0038a811e76306c7ca20b131513e0b2877
z3tr <m5tfi@pm.me>
2020-12-27 21:21:42 +0200

    assets can be scaled through JSON file and getting player's starting position from the JSON file

--- src/levelloader.py
+++ src/levelloader.py
@@ -13,14 +13,15 @@ class LevelLoader:
         self.__level_id = level_id
         self.__assets_file = self.__load_assets_file()
 
-        self.__player_images = self.__load_player_images()
-        self.__level_spritesheet = self.__load_level_spritesheet()
+        self.__player_spritesheet = self.__load_spritesheet('player')
+        self.__player_images = self.__player_spritesheet.sprites
+        self.__level_spritesheet = self.__load_spritesheet('tiles')
         self.__level_map = self.__load_level_map()
         self.__sounds = self.__load_sounds()
         self.__fonts = self.__load_fonts()
 
     def __load_assets_file(self):
-        assets_file_path = f'levels/L{self.__level_id}/assets.json'
+        assets_file_path = f'assets/levels/L{self.__level_id}/assets.json'
         with open(assets_file_path, 'r') as f:
             data = f.read()
         self.logger.info('Assets file is loaded')
@@ -31,35 +32,24 @@ class LevelLoader:
         self.logger.info("Fonts are loaded")
         return fonts
 
-    def __load_player_images(self):
-        player_images = []
-        for i in self.__assets_file['player']:
-            player_images.append(self.load_image(f'assets/{i}'))
-        self.logger.info("Player images are loaded")
-        return player_images
-
-    def __load_level_spritesheet(self):
+    def __load_spritesheet(self, object_name: str):
         # ss_obj stands for spritesheet json object
-        ss_obj = self.__assets_file['tiles']
+        ss_obj = self.__assets_file[object_name]
         ss_filepath = f'assets/{ss_obj["filepath"]}'
-        ss_tile_size_x = int(ss_obj['tile_size_x'])
-        ss_tile_size_y = int(ss_obj['tile_size_y'])
-        return SpriteSheet(ss_filepath, ss_tile_size_x, ss_tile_size_y)
+        ss_tile_size = ss_obj['tile_size']
+        ss_render_tile_size = ss_obj['render_tile_size']
+        return SpriteSheet(ss_filepath, ss_tile_size, ss_render_tile_size)
 
     def __load_level_map(self):
-        level_map_path = f'levels/L{self.__level_id}/map.csv'
-        return TileMap(level_map_path, self.__level_spritesheet)
+        level_map_path = f'assets/levels/L{self.__level_id}/map.csv'
+        player_tile_id = self.__assets_file['tiles']['player_tile_id']
+        render_tile_size = self.__assets_file['tiles']['render_tile_size']
+        return TileMap(level_map_path, self.__level_spritesheet, render_tile_size, player_tile_id)
 
     def __load_sounds(self):
         # TODO: load sounds
         # self.logger.info("Sounds are loaded")
         return []
-
-    @staticmethod
-    def load_image(image_path: str, with_alpha: bool = True):
-        if with_alpha:
-            return pygame.image.load(image_path).convert_alpha()
-        return pygame.image.load(image_path).convert()
 
     def get_player_images(self):
         return self.__player_images
--- src/spritesheet.py
+++ src/spritesheet.py
@@ -7,29 +7,36 @@ class SpriteSheet:
     Later on, we can retrieve the specific sprite by id
     """
 
-    def __init__(self, filename: str, sprite_width: int, sprite_height: int):
+    def __init__(self, filename: str, sprite_size: tuple[int, int], render_size: tuple[int, int]):
         self.__filename = filename
-        self.__sprite_width = sprite_width
-        self.__sprite_height = sprite_height
+        self.__sprite_size = sprite_size
         self.__sprite_sheet = pygame.image.load(filename).convert_alpha()
+        self.__sprite_sheet.set_colorkey((0, 0, 0))
         self.__rect = self.__sprite_sheet.get_rect()
-        self.__sprites = self.__get_all_sprites()
+        self.__sprites = self.__get_all_sprites(render_size)
 
-    def __get_sprite(self, x, y) -> pygame.Surface:
-        sprite = pygame.Surface((self.__sprite_width, self.__sprite_height))
-        sprite_rect = pygame.rect.Rect(x, y, self.__sprite_width, self.__sprite_height)
+    def __get_sprite(self, location: tuple[int, int], render_size: tuple[int, int]) -> pygame.Surface:
+        sprite = pygame.Surface(self.__sprite_size)
+        sprite_rect = pygame.rect.Rect(*location, *self.__sprite_size)
         sprite.blit(self.__sprite_sheet, (0, 0), sprite_rect)
+        sprite = pygame.transform.scale(sprite, render_size)
+        sprite.set_colorkey((0, 0, 0))
         return sprite
 
-    def __get_all_sprites(self):
+    def __get_all_sprites(self, render_size):
         sprites = []
-        columns = self.__rect.w // self.__sprite_width
-        rows = self.__rect.h // self.__sprite_height
+        columns = self.__rect.w // self.__sprite_size[0]
+        rows = self.__rect.h // self.__sprite_size[1]
 
         for r in range(rows):
             for c in range(columns):
-                sprites.append(self.__get_sprite(c * self.__sprite_width, r * self.__sprite_height))
+                location = c * self.__sprite_size[0], r * self.__sprite_size[1]
+                sprites.append(self.__get_sprite(location, render_size))
         return sprites
 
     def get_sprite_by_id(self, sprite_id: int) -> pygame.Surface:
         return self.__sprites[sprite_id]
+
+    @property
+    def sprites(self):
+        return self.__sprites
