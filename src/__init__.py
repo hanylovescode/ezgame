248575c579ff7521694680744d1d8f7e10cc747e
z3tr <m5tfi@pm.me>
2020-12-30 19:39:37 +0200

    add __init__.py file

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+This file is left empty on purpose; I wanted to be able to import the
+src package somewhere else and the easiest way is to create an empty
+__init__.py file.
+"""
+# reference: https://stackoverflow.com/a/21995949
+# TODO: look up the main purpose of __init__.py file
